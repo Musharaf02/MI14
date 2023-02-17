d2ed4c194f1e3043a5c45dc9d8fde0d00113d9ae
Mohamed Musharaf <98466197+Musharaf02@users.noreply.github.com>
2023-02-17 18:54:02 +0530

    Delete WEB directory

--- WEB/app.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector(".menu");
-let links = document.querySelector(".navlinks");
-// let maincontent = document.querySelector(".main-content");
-
-
-menu.addEventListener('click',()=>{
-    links.classList.toggle('hide')
-    // maincontent.classList.toggle('moveleft')
-})
