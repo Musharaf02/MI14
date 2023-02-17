d3af4644e9ac2e92c690f0b548f922e81b4deb06
Mohamed Musharaf <98466197+Musharaf02@users.noreply.github.com>
2023-02-17 18:47:02 +0530

    commitweb

--- WEB/app.js
+++ WEB/app.js
@@ -1,9 +1,9 @@
 let menu = document.querySelector(".menu");
 let links = document.querySelector(".navlinks");
-let maincontent = document.querySelector(".main-content");
+// let maincontent = document.querySelector(".main-content");
 
 
 menu.addEventListener('click',()=>{
     links.classList.toggle('hide')
-    maincontent.classList.toggle('moveleft')
+    // maincontent.classList.toggle('moveleft')
 })
